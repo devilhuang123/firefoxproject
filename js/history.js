774a17d6437eb21d54bb37f290128e6d148238f7
heran7 <heran963@gmail.com>
2013-12-08 18:30:08 +0800

    history_v1

--- js/history.js
+++ js/history.js
@@ -1,3 +1,11 @@
 function his_print(){
-	document.write("this is history's page");
+		var sum = 10;
+		var succeed = 7;
+		var fail = 2;
+		var notstart = 1;
+		var succeesrate = (succeed / sum)*100;
+		
+		document.write("歷史數據" +　"<br>");
+		document.write("現已完成任務" + sum + "次，其中成功" + succeed + "次，失敗" + fail + "次，未開始" + notstart + "次。" + "<br>");
+		document.write("任務成功率為" + succeesrate + "%。")
 }
