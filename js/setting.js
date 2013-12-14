5bc4a4b5c7a51a1a7075e2b97feff3a8f785de1f
JoeyChiou <joeyc0916@gmail.com>
2013-12-15 01:08:29 +0800

    Publish Setting.Mode v2 & Setting.Type v1
    finish Type model
    update index.html -- update Mode UI
    update setting.js -- add Type functions
    create task_type.xml -- task dom
    add annotations

--- js/setting.js
+++ js/setting.js
@@ -1,14 +1,28 @@
 var mode = "sweet";
 
+//Constructor
 function setting(){
 	document.getElementById("mode_table").style.display = "none";
 	document.getElementById("task_type_table").style.display = "none";
 	document.getElementById("history_manage_table").style.display = "none";
 }
+
 function Mode(){
 	document.getElementById("mode_table").style.display = null;
+	document.getElementById("task_type_table").style.display = "none";
+	document.getElementById("history_manage_table").style.display = "none";
+	getMode();
 }
 
+function Type(){
+	document.getElementById("mode_table").style.display = "none";
+	document.getElementById("task_type_table").style.display = null;
+	document.getElementById("history_manage_table").style.display = "none";
+}
+
+///
+//Mode functions -- getMode(),setMode().
+///
 function getMode(){
 	if(mode == "sweet")
 		document.getElementById("td_mode").textContent = "溫柔模式";
@@ -28,3 +42,37 @@ function setMode(){
 		alert("您選擇了"+option[index].text);
 	}
 }
+///
+//Mode Functions end
+///
+
+///
+//Type Functions
+///
+function getOption(){
+	var options = ["吃飯","睡覺","寫作業","打林宥均","親鄭永斌教授"];
+	
+	/*
+	var doc = new ActiveXObject("Msxml2.DOMDocument");
+	doc.load("./task_type.xml");
+	alert(doc.xml);
+	var root = doc.documentElement;
+	var nodelist = root.childNodes;
+
+	for(var i=0;i<nodelist.length;i++){
+		alert(nodelist[i].value);
+	}
+	*/
+}
+
+function appendOption(){
+	var option = document.createElement('option');
+	option.text = document.getElementById("text_type").value;
+	option.value = document.getElementById("text_type").value;
+	
+	var select_type = document.getElementById('select_type');
+	select_type.add(option, null);
+}
+///
+//Type Functions end
+///
