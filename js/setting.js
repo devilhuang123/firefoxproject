96bf57406aa9049773451dc8dd7e3c4527628898
JoeyChiou <joeyc0916@gmail.com>
2013-12-14 17:42:49 +0800

    Publish Setting.Mode v1
    finish Mode model
    update index.html
    create setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,8 +1,30 @@
-var status=0;
+var mode = "sweet";
 
-function set_print(){
-	document.write("this is setting's page");
+function setting(){
+	document.getElementById("mode_table").style.display = "none";
+	document.getElementById("task_type_table").style.display = "none";
+	document.getElementById("history_manage_table").style.display = "none";
 }
-function getStatus(){
-	return status;
+function Mode(){
+	document.getElementById("mode_table").style.display = null;
+}
+
+function getMode(){
+	if(mode == "sweet")
+		document.getElementById("td_mode").textContent = "溫柔模式";
+	else
+		document.getElementById("td_mode").textContent = "剽悍模式";
+}
+
+function setMode(){
+	var index = document.getElementById("select_mode").selectedIndex;
+	var option = document.getElementById("select_mode").options;
+	
+	if(option[index].text == "溫柔模式"){
+		document.getElementById("td_mode").textContent = "溫柔模式";
+		alert("您選擇了"+option[index].text);
+	}else{
+		document.getElementById("td_mode").textContent = "剽悍模式";
+		alert("您選擇了"+option[index].text);
+	}
 }
