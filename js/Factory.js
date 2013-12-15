648617c2608a8514f6a8dfc0576462e514afa8ae
Cabris <christtabris@gmail.com>
2013-12-16 02:03:58 +0800

    format code

--- js/Factory.js
+++ js/Factory.js
@@ -1,32 +1,30 @@
-function ElementFactory(_document){
-	ElementFactory.Document=_document;
+function ElementFactory(_document) {
+	ElementFactory.Document = _document;
 }
 
-ElementFactory.Document=null;
+ElementFactory.Document = null;
 
-ElementFactory.CraeteElement=function(type){
-		return ElementFactory.Document.createElement(type);
+ElementFactory.CraeteElement = function(type) {
+	return ElementFactory.Document.createElement(type);
 };
 
-ElementFactory.LoadCSS=function(href){
+ElementFactory.LoadCSS = function(href) {
 	var css = jQuery("<link>");
-    css.attr({
-      rel:  "stylesheet",
-      type: "text/css",
-      href: href
-    });
-    Assert(css!=null,"Load css failed");
-    $("head").append(css);
-    return css;
+	css.attr({
+		rel : "stylesheet",
+		type : "text/css",
+		href : href
+	});
+	Assert(css != null, "Load css failed");
+	$("head").append(css);
+	return css;
 };
 
-ElementFactory.LoadScript=function(href){
-	var script=$.getScript(href,function(data, textStatus, jqxhr){
+ElementFactory.LoadScript = function(href) {
+	var script = $.getScript(href, function(data, textStatus, jqxhr) {
 	});
-	Assert(script!=null,"Load script failed");
+	Assert(script != null, "Load script failed");
 	return script;
 };
-
-
 
 ElementFactory(document);
--- js/calendar.js
+++ js/calendar.js
@@ -1,51 +1,47 @@
-var  g_globalObject2;
+var g_globalObject2;
 var calenderDiv;
 var messageDiv;
-var calendarId="datepicker";
-var messageId="myMessage";
+var calendarId = "datepicker";
+var messageId = "myMessage";
 
-function ShowCalendar(_div){
-	
+function ShowCalendar(_div) {
+
 	ElementFactory.LoadCSS("css/ui-darkness/jquery-ui-1.10.3.custom.css");
 	ElementFactory.LoadScript("js/jquery-ui-1.10.3.custom.min.js");
 	//var _div=ElementFactory.CraeteElement("div");
 	calenderDiv = _div;
-	calendarDiv=AddDiv(_div,'#A2A1EF');
-	calendarDiv.id=calendarId;
+	calendarDiv = AddDiv(_div, '#A2A1EF');
+	calendarDiv.id = calendarId;
 
-    messageDiv =_div;
-    messageDiv = AddDiv(_div,'#E76DA9');
-	messageDiv.id=messageId;
+	messageDiv = _div;
+	messageDiv = AddDiv(_div, '#E76DA9');
+	messageDiv.id = messageId;
 	Initialize();
 }
 
-
-function AddDiv(objTo,_color) {
-    var divtest = ElementFactory.CraeteElement("div");
-    objTo.appendChild(divtest);
-    divtest.style.background = _color; 
-    // divtest.style.overflow="hidden";
-    // divtest.style.clear="both";
-    // divtest.style.height="100%";
-    // divtest.style.width="100%";
-     //divtest.style.width=width;
-    // divtest.style.height=height;
-    return divtest;
+function AddDiv(objTo, _color) {
+	var divtest = ElementFactory.CraeteElement("div");
+	objTo.appendChild(divtest);
+	divtest.style.background = _color;
+	// divtest.style.overflow="hidden";
+	// divtest.style.clear="both";
+	// divtest.style.height="100%";
+	// divtest.style.width="100%";
+	//divtest.style.width=width;
+	// divtest.style.height=height;
+	return divtest;
 }
 
-function Initialize(){
-		 $(function() {
-    		$( "#datepicker" ).datepicker(
-    			{ autoSize: true, 
-    			onSelect:OnDateSelected
-    			}
-    		);
-  		});	
+function Initialize() {
+	$(function() {
+		$("#datepicker").datepicker({
+			autoSize : true,
+			onSelect : OnDateSelected
+		});
+	});
 }
 
-function OnDateSelected(dateText, inst){
-			messageDiv.innerHTML = dateText;
+function OnDateSelected(dateText, inst) {
+	messageDiv.innerHTML = dateText;
 }
-
-	
 
