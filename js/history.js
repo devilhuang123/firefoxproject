c8e92b16da6bf9c088f5fb02f60c6ba2a01cfe34
heran7 <heran963@gmail.com>
2014-01-04 19:51:35 +0800

    circle_test

--- js/history.js
+++ js/history.js
@@ -1,110 +1,84 @@
-/*function his_print(){
-	
-	var task = doc.getElementsByTagName("Task");
-
-	if(task.length==0)
-	{
-		document.write("現在沒有完成任何任務" + "<br>");
-	}
-	else
-	{
-		document.write("現已完成" + task.length + "次任務" + "<br>");
-		//我這邊好像也需要吃到任務的type，否則無法判定每一個task裡面的Type和什麽做對比，決定每一種任務類型分別的成功失敗結果。
-	}
-	
-}*/
-/*
-function printtable(){
-	
-	document.write("<table border='1'>");
-	var task = doc.getElementsByTagName("Task");
-	
-	document.write("<tr><td>");
-	document.write("任務類型");
-	document.write("</td><td>");
-	document.write("完成狀態");
-	document.write("</td></tr>");
-	
-	for (i=0;i<task.length;i++)
-	{
-  		document.write("<tr><td>");
-  		document.write(task[i].getElementsByTagName("Type")[0].childNodes[0].nodeValue);
-  		document.write("</td><td>");
-  		if(task[i].getElementsByTagName("EndTime")==0)
-  		{
-  			document.write("未完成");
-  		}
-  		else
-  		{
-  			//保存那三個時間的時候具體是怎麼存？需要判斷是否完成
-  		}
-  		document.write("</td></tr>");
-	}
-	
-	document.write("</table>");
-}
-*/
-/*function his_print(){
-	if(cptTasks.length==0)
-	{
-		document.write("現在沒有完成任何任務" + "<br>");
-	}
-	else
-	{
-		n = cptTasks.length - 1;
-		document.write("現已完成" + n + "次任務" + "<br>");
-			
-		typen = taskTypes.length - 1;
-		for (var i=0; i<typen; i++)
-		{
-			printType(taskTypes[i]);
-		}
-	}
-}
-
-
-//print everytype result
-function printType(type){
-	
-	var sum = 0;
-	var successNo = 0;
-	var failNo = 0;
-	
-	cptn = cptTasks.length - 1;
-	for (var i=0; i<cptn; i++){	
+function getSelectedChange()
+{
+	var selected = document.getElementById('history_type');
+	//alert(selected.value);
+	//showCircle(selected.value);
+	if(selected.value == "吃飯")
+		drawCircle("canvas_circle", data_arr_eat, color_arr, text_arr);
+	else if	(selected.value == "睡覺")
+		drawCircle("canvas_circle", data_arr_sleep, color_arr, text_arr);
+	else 
+		drawCircle("canvas_circle", data_arr_class, color_arr, text_arr);
 		
-		if(cptTasks[i].Type == type)
-		{
-			sum = sum + 1;
-			if(cptTasks[i].Result == "SUCCESS")
-			{
-				successNo = successNo + 1;
-			}
-			else if(cptTasks[i].Result == "FAIL")
-			{
-				failNo = failNo + 1;
-			}
-			else
-			{
-			assert();
-			}
-		}
-		else
-		{
-			assert();
-		}
-	}
+}
+
+
+	var color_arr = ["#00FF21", "#FFAA00", "#00AABB"];  
+	var text_arr = ["Success", "Fail", "Undo"]; 
+	var data_arr_eat = [0.8, 0.1, 0.1];
+	var data_arr_sleep = [0.5, 0.5, 0];
+	var data_arr_class = [0, 0, 0];
+
+/*function showCircle(type)
+{
+	var color_arr = ["#00FF21", "#FFAA00", "#00AABB"];  
+	var text_arr = ["Success", "Fail", "Undo"]; 
+	var data_arr;
 	
-	if (sum == 0)
-	{
-		document.write("尚未進行"+ type + "任務" + "<br>");
-	}
-	else
-	{
-		document.write("任務類型：" + type + "<br>");
-		document.write("其中成功" + successNo + "次，失敗" + failNo + "次，任務成功率為" + successNo/sum + "。");
-	}
+	//抓某一type的完成任务中的资料，写入data_arr中
+	
+	drawCircle("canvas_circle", data_arr, color_arr, text_arr);
 }*/
+
+//绘制饼图  
+function init() {  
+	
+	var data_arr = [0.15, 0.25, 0.6];  
+	var color_arr = ["#00FF21", "#FFAA00", "#00AABB"];  
+	var text_arr = ["success", "fail", "第三季度"]; 
+	
+    drawCircle("canvas_circle", data_arr, color_arr, text_arr);  //绘制饼图  比例数据和颜色 
+} 
+
+function drawCircle(canvasId, data_arr, color_arr, text_arr)  
+{  
+    var c = document.getElementById(canvasId);  
+    var ctx = c.getContext("2d");  
+  
+    var radius = c.height / 2 - 20;  
+    var ox = radius + 20, oy = radius + 20; //圆心  
+  
+    var width = 30, height = 10; //图例宽和高  
+	var posX = ox * 2 + 20, posY = 30;   //  
+    var textX = posX + width + 5, textY = posY + 10;  
+  
+    var startAngle = 0; //起始弧度  
+	var endAngle = 0;   //结束弧度
+	  
+    for (var i = 0; i < data_arr.length; i++)  
+    {  
+        //绘制饼图  
+        endAngle = endAngle + data_arr[i] * Math.PI * 2; //结束弧度  
+        ctx.fillStyle = color_arr[i];  
+        ctx.beginPath();  
+        ctx.moveTo(ox, oy); //移动到到圆心  
+        ctx.arc(ox, oy, radius, startAngle, endAngle, false);  
+        ctx.closePath();  
+        ctx.fill();  
+        startAngle = endAngle; //设置起始弧度  
+  
+                    //绘制比例图及文字  
+        ctx.fillStyle = color_arr[i];  
+        ctx.fillRect(posX, posY + 20 * i, width, height);  
+        ctx.moveTo(posX, posY + 20 * i);  
+        ctx.font = '12px';    //斜体 30像素 微软雅黑字体  
+        ctx.fillStyle = color_arr[i]; //"#000000";  
+        var percent = text_arr[i] + "：" + 100 * data_arr[i] + "%";  
+        ctx.fillText(percent, textX, textY + 20 * i);  
+    }  
+}  
+
+
 /*
 //delete history
 function hisdel(){
